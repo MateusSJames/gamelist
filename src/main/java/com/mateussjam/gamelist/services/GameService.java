92c5812d45f1374cc16d886b188aa37e1afdf69d
MateusSJames <mateusaristides23@gmail.com>
2024-11-12 17:59:49 -0300

    feat: get game by id

--- src/main/java/com/mateussjam/gamelist/services/GameService.java
+++ src/main/java/com/mateussjam/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mateussjam.gamelist.dto.GameDTO;
 import com.mateussjam.gamelist.dto.GameMinDTO;
 import com.mateussjam.gamelist.entities.Game;
 import com.mateussjam.gamelist.repositories.GameRepository;
@@ -14,6 +16,13 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
